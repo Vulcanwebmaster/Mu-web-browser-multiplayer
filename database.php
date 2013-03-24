049d8552902f2affc652529f141a9310a073f83e
Vulcanwebmaster <vulcanwebmaster@hotmail.com>
2013-03-23 22:43:54 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysql_connect("187.45.196.198", "abreunet", "Lm923885") or die(mysql_error());
-mysql_select_db('abreunet', $conn) or die(mysql_error());
+$conn = mysql_connect("your-ip", "user", "pass") or die(mysql_error());
+mysql_select_db('database-name', $conn) or die(mysql_error());
 ?>
